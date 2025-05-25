16653ebdb8bcb98d14bc0548cb3cd0514a699cd8
lucnd-se180732 <lucndse180732@fpt.edu.vn>
2025-05-25 19:08:28 +0700

    upload project

---
+++ src/main/java/com/genx/SwpDnaGenxApplication.java
@@ -0,0 +1,13 @@
+package com.genx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwpDnaGenxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwpDnaGenxApplication.class, args);
+    }
+
+}
